db2cc12c1101874c69f08f08ed6fad679a4eef04
Mayito vicente <cv2143304@gmail.com>
2024-04-12 19:17:39 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+function bubbleSort(arr) {
+    var len = arr.length;
+    for (var i = 0; i < len; i++) {
+        for (var j = 0; j < len - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Intercambiar elementos
+                var temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+var palabras = ["a", "e", "i", "u", "o", "f", "v"];
+console.log("Antes de ordenar:", palabras);
+console.log("Después de ordenar:", bubbleSort(palabras));
