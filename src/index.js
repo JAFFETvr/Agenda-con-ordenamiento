3e501ac6eaa41b82c88a81c42240a9d832f505be
Mayito vicente <cv2143304@gmail.com>
2024-04-12 19:25:04 -0600

    nueva cosa

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ function bubbleSort(arr) {
     for (var i = 0; i < len; i++) {
         for (var j = 0; j < len - 1; j++) {
             if (arr[j] > arr[j + 1]) {
-                // Intercambiar elementos
                 var temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
